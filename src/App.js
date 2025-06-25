ff82bea4f6af50ef183f10fbb7b8734d4b2d96af
zlhsny <126148560+zulhusni2003@users.noreply.github.com>
2025-06-25 08:53:07 +0800

    Clean Project Before Design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// Import the React library, so can use JSX & React features
+import React from 'react';
 
+// Functional component called App
 function App() {
+  // The component returns some HTML-like code (JSX)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
 }
 
+// This makes the App component available to be used in other files
 export default App;

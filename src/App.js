5ab1ae446eade23768db6930cd08b1c5539a9ef9
zlhsny <126148560+zulhusni2003@users.noreply.github.com>
2025-06-28 16:08:26 +0800

    Formation HomePage & PageTop Setup

--- src/App.js
+++ src/App.js
@@ -1,39 +1,14 @@
-// Import React so we can use JSX and build components
 import React from 'react';
-
-// Import the component we made
-import TopBanner from './components/TopBanner/TopBanner';
-import Services from './components/Services/Services';
 import TopNavigation from './components/TopNavigation/TopNavigation';
-import Analysis from './components/Analysis/Analysis';
-import Summary from './components/Summary/Summary';
-import RecentProject from './components/RecentProject/RecentProject';
-import Courses from './components/Courses/Courses';
-import Video from './components/Video/Video';
-import ClientReview from './components/ClientReview/ClientReview';
-import AboutMe from './components/AboutMe/AboutMe';
-import Footer from './components/Footer/Footer';
+import PageTop from './components/PageTop/PageTop';
 
-// Create a functional component named App
 function App() {
-  // Return some JSX (HTML-like code) to show on the web page
   return (
     <div>
-      {/* Show the TopBanner component here */}
       <TopNavigation />
-      <TopBanner />
-      <Services />
-      <Analysis />
-      <Summary />
-      <RecentProject />
-      <Courses />
-      <Video />
-      <ClientReview />
-      <AboutMe />
-      <Footer />
+      <PageTop pagetitle="About Me" />
     </div>
   );
 }
 
-// Export this App component so it can be used in other files
 export default App;

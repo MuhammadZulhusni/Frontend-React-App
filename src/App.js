6bde18f115cdae975c0335523779501bb99de4ce
zlhsny <126148560+zulhusni2003@users.noreply.github.com>
2025-06-28 16:28:09 +0800

    Formation Course Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import AboutPage from './pages/AboutPage';
+import AllCoursePage from './pages/AllCoursePage';
  
 
 function App() {
   return (
     <div>
     
-     <AboutPage />
+     <AllCoursePage />
       
     </div>
   );

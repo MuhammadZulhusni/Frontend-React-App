d7c2221c93457a608b64f22830c3403962d88e24
zlhsny <126148560+zulhusni2003@users.noreply.github.com>
2025-07-01 01:36:48 +0800

    Axios Test

--- src/components/TopBanner/TopBanner.jsx
+++ src/components/TopBanner/TopBanner.jsx
@@ -4,8 +4,24 @@ import React, { Component, Fragment } from 'react'
 // Import layout and button components from react-bootstrap
 import { Col, Container, Row, Button } from 'react-bootstrap'
 
+import axios from 'axios';
+
 // Create a class component called TopBanner
 class TopBanner extends Component {
+
+    // This method runs automatically when the component is mounted (inserted into the DOM)
+    componentDidMount() {
+    // Send a GET request to the Laravel API endpoint to get homepage title data
+    axios.get('http://127.0.0.1:8000/api/homepage/title')
+        .then(function (response) {    
+        // If successful, log the response data to the console
+        console.log(response.data);
+        })
+        .catch(function (error) {
+        // If there's an error, log the error to the console
+        console.log(error);
+        });
+    }
 
     // render() decides what to show on the screen
     render() {

195b9db6e24becb37bca889e822c66c94e2865b8
zlhsny <126148560+zulhusni2003@users.noreply.github.com>
2025-07-06 07:12:59 +0800

    dot before loading for TopBanner

--- src/components/TopBanner/TopBanner.jsx
+++ src/components/TopBanner/TopBanner.jsx
@@ -1,72 +1,77 @@
-// Import necessary tools from React
+// Import required tools from React
 import React, { Component, Fragment } from 'react'
 
 // Import layout and button components from react-bootstrap
 import { Col, Container, Row, Button } from 'react-bootstrap'
 
-// Import API helper files
+// Import axios (not used here but can be used for API requests)
+import axios from 'axios';
+
+// Import helper files for API request
 import RestClient from '../../RestAPI/RestClient';
 import AppUrl from '../../RestAPI/AppUrl';
 
-// Import axios for HTTP requests (not used here, but imported)
-import axios from 'axios';
-
-// Create a React class component called TopBanner
+// Create a class component named TopBanner
 class TopBanner extends Component {
 
-    // Constructor to set initial state
-    constructor(){
-        super();
-        this.state={
-            title:"",     // Title text from API
-            subtitle:""   // Subtitle text from API
-        }
-    }
+     // Initialize state with default title and subtitle
+     constructor(){
+          super();
+          this.state={
+               title:".....",      // Default title before loading
+               subtitle:"....."    // Default subtitle before loading
+          }
+     }
 
-    // This runs after the component is shown on the screen
-    componentDidMount(){
-        // Get data from API and update the state
-        RestClient.GetRequest(AppUrl.HomeTopTitle).then(result=>{
-            this.setState({
-                title: result[0]['home_title'],
-                subtitle: result[0]['home_subtitle']
-            });
-        });
-    }
+     // This function runs after the component is shown on the screen
+     componentDidMount(){
+          // Get data from API and update the state
+          RestClient.GetRequest(AppUrl.HomeTopTitle).then(result=>{
+               this.setState({
+                    title: result[0]['home_title'],
+                    subtitle: result[0]['home_subtitle']
+               });
+          }).catch(error=>{
+              // If there's an error, show fallback text
+              this.setState({
+                  title: "????",
+                  subtitle: "????"
+              })
+          });
+     }
 
-    // This decides what will be shown in the browser
-    render() {
-        return (
-            // Fragment lets us return multiple elements without extra HTML tag
-            <Fragment>
+     // Renders the component UI
+     render() {
+          return (
+               <Fragment>
+                    {/* Full-width banner section */}
+                    <Container fluid={true} className="topFixedBanner p-0" >
+                         
+                         {/* Overlay on top of banner image */}
+                         <div className="topBannerOverlay">
 
-                {/* Full-width banner section */}
-                <Container fluid={true} className="topFixedBanner p-0">
-                    
-                    {/* Dark overlay over the banner image */}
-                    <div className="topBannerOverlay">
-                        
-                        {/* Inner container to center the text content */}
-                        <Container className="topContent">
-                            <Row>
-                                <Col className="text-center">
+                              {/* Inner content area for text */}
+                              <Container className="topContent">
+                                   <Row>
+                                        <Col className="text-center">
+                                            
+                                            {/* Show title and subtitle from state */}
+                                            <h1 className="topTitle">{this.state.title}</h1>
+                                            <h4 className="topSubTitle">{this.state.subtitle}</h4>
+                                            
+                                            {/* Button below the text */}
+                                            <Button variant="primary">Learn More</Button>
 
-                                    {/* Dynamic title and subtitle from API */}
-                                    <h1 className="topTitle">{this.state.title}</h1>
-                                    <h4 className="topSubTitle">{this.state.subtitle}</h4>
-                                    
-                                    {/* A button below the text */}
-                                    <Button variant="primary">Learn More</Button>
-                                </Col>
-                            </Row>
-                        </Container>
+                                        </Col>
+                                   </Row> 
+                              </Container> 
+                         </div>
 
-                    </div>
-                </Container>
-            </Fragment>
-        )
-    }
+                    </Container>
+               </Fragment>
+          )
+     }
 }
 
-// Export this component so other files can use it
+// Export this component so it can be used in other files
 export default TopBanner

3d83b693bdd932b7adfcb87a36b2b5ef2a9fa442
zlhsny <126148560+zulhusni2003@users.noreply.github.com>
2025-07-07 16:39:14 +0800

    API Consume For Courses at omepage

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,111 +1,79 @@
-import React, { Component, Fragment } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-
+// Import React and needed components
+import React, { Component, Fragment } from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
+// Import API helper files
+import RestClient from '../../RestAPI/RestClient';
+import AppUrl from '../../RestAPI/AppUrl';
+
+// Create a class component called Courses
 class Courses extends Component {
-  render() {
-    return (
-      <Fragment>
-        {/* Main container with center-aligned content */}
-        <Container className="text-center">
 
-          {/* Section title */}
-          <h1 className="serviceMainTitle">MY COURSES</h1>
+     // Set initial state
+     constructor(){
+          super();
+          this.state = {
+               myData: [] // This will hold the course data from the API
+          }
+     }
 
-          {/* Decorative line below title */}
-          <div className="bottom"></div>
+     // This function runs after the component is added to the page
+     componentDidMount(){
+          // Get course data from the API and update the state
+          RestClient.GetRequest(AppUrl.CourseHome).then(result => {
+               this.setState({ myData: result });
+          }) 
+     }
 
-          {/* Main row containing 2 columns (each column has 2 courses) */}
-          <Row>
-            
-            {/* === Left column === */}
-            <Col lg={6} md={12} sm={12}>
-              <Row>
-                {/* Course 1 - Image */}
-                <Col lg={6} md={6} sm={12} className="p-2">
-                  <img
-                    className="courseImg"
-                    src="https://image.freepik.com/free-photo/learner-lesson_1098-14155.jpg"
-                    alt="Laravel course one"
-                  />
-                </Col>
+     // This function decides what to display on the page
+     render() {
+          // Store the course list from API
+          const MyList = this.state.myData;
 
-                {/* Course 1 - Info */}
-                <Col lg={6} md={6} sm={12}>
-                  <h5 className="text-justify serviceName">Laravel 8</h5>
-                  <p className="text-justify serviceDescription">
-                    Some quick example text to build on the card title and make up the bulk of the card's content.
-                  </p>
-                  <Link className="courseViewMore float-left" to="/coursedetails" >View Details</Link>
-                </Col>
+          // Map through the course list and create UI elements
+          const MyView = MyList.map(MyList => {
+            return (
+              <Col lg={6} md={12} sm={12}>
+                <Row>
+                  {/* Left side - image */}
+                  <Col lg={6} md={6} sm={12} className="p-2">
+                    <img className="courseImg" src={MyList.small_img} />
+                  </Col>
 
-                {/* Course 2 - Image */}
-                <Col lg={6} md={6} sm={12} className="p-2">
-                  <img
-                    className="courseImg"
-                    src="https://image.freepik.com/free-photo/coach-by-whiteboard_1098-12970.jpg"
-                    alt="Laravel course two"
-                  />
-                </Col>
+                  {/* Right side - title, description, and link */}
+                  <Col lg={6} md={6} sm={12}>
+                    <h5 className="text-justify serviceName">{MyList.short_title}</h5>
+                    <p className="text-justify serviceDescription">{MyList.short_description}</p>
+                    <Link className="courseViewMore float-left" to="/coursedetails">
+                      View Details
+                    </Link>
+                  </Col>
+                </Row>
+              </Col>
+            )
+          })
 
-                {/* Course 2 - Info */}
-                <Col lg={6} md={6} sm={12}>
-                  <h5 className="text-justify serviceName">Laravel 8</h5>
-                  <p className="text-justify serviceDescription">
-                    Some quick example text to build on the card title and make up the bulk of the card's content.
-                  </p>
-                  <Link className="courseViewMore float-left" to="/coursedetails" >View Details</Link>
-                </Col>
-              </Row>
-            </Col>
+          // Return the full layout
+          return (
+            <Fragment>
+              {/* Main container for courses section */}
+              <Container className="text-center">
+                
+                {/* Section title */}
+                <h1 className="serviceMainTitle">MY COURSES</h1>
+                <div className="bottom"></div>
 
-            {/* === Right column === */}
-            <Col lg={6} md={12} sm={12}>
-              <Row>
-                {/* Course 3 - Image */}
-                <Col lg={6} md={6} sm={12} className="p-2">
-                  <img
-                    className="courseImg"
-                    src="https://image.freepik.com/free-photo/shocked-male-student-poses-desktop-home-office-uses-laptop-computer-searching-online-education-course-browses-distance-learning-website_273609-34548.jpg"
-                    alt="Laravel course three"
-                  />
-                </Col>
+                {/* Display course cards */}
+                <Row>
+                  {MyView}
+                </Row>
 
-                {/* Course 3 - Info */}
-                <Col lg={6} md={6} sm={12}>
-                  <h5 className="text-justify serviceName">Laravel 8</h5>
-                  <p className="text-justify serviceDescription">
-                    Some quick example text to build on the card title and make up the bulk of the card's content.
-                  </p>
-                  <Link className="courseViewMore float-left" to="/coursedetails" >View Details</Link>
-                </Col>
-
-                {/* Course 4 - Image */}
-                <Col lg={6} md={6} sm={12} className="p-2">
-                  <img
-                    className="courseImg"
-                    src="https://image.freepik.com/free-photo/training-managers_1098-16067.jpg"
-                    alt="Laravel course four"
-                  />
-                </Col>
-
-                {/* Course 4 - Info */}
-                <Col lg={6} md={6} sm={12}>
-                  <h5 className="text-justify serviceName">Laravel 8</h5>
-                  <p className="text-justify serviceDescription">
-                    Some quick example text to build on the card title and make up the bulk of the card's content.
-                  </p>
-                  <Link className="courseViewMore float-left" to="/coursedetails" >View Details</Link>
-                </Col>
-              </Row>
-            </Col>
-
-          </Row>
-        </Container>
-      </Fragment>
-    );
-  }
+              </Container>
+            </Fragment>
+          )
+     }
 }
 
-export default Courses;
+// Export the component so it can be used in other files
+export default Courses

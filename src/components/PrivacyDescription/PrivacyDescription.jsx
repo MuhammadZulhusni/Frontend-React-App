667625391463279cbf5e32fecedab228eab030ea
zlhsny <126148560+zulhusni2003@users.noreply.github.com>
2025-07-08 18:25:23 +0800

    API Consume Policy

--- src/components/PrivacyDescription/PrivacyDescription.jsx
+++ src/components/PrivacyDescription/PrivacyDescription.jsx
@@ -1,56 +1,52 @@
-import React, { Component, Fragment } from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
+// Import necessary modules from React and Bootstrap
+import React, { Component, Fragment } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+
+// Import custom API client and API URL
+import RestClient from '../../RestAPI/RestClient';
+import AppUrl from '../../RestAPI/AppUrl';
+
+// Import modern HTML parser to render HTML content as React elements
+import parse from 'html-react-parser';
 
 class PrivacyDescription extends Component {
-  render() {
-    return (
-      <Fragment>
-        {/* Main container for Privacy & Policy content with top margin */}
-        <Container className="mt-5">
-          <Row>
-            {/* Full-width column to display content */}
-            <Col lg={12} md={12} sm={12}>
-              {/* Section heading */}
-              <h1 className="serviceName">Privacy And Policy</h1>
-              <hr />
+     constructor() {
+          super();
+          // Initialize state with default placeholder
+          this.state = { 
+               privacydesc: "..."
+          };
+     }
 
-              {/* Paragraph with details about privacy policies */}
-              <p className="serviceDescription">
-                {/* Comments section */}
-                <b>Comments</b> <br />
-                When visitors leave comments on the site, we collect the data shown in the comments form, and also the visitor’s IP address and browser user agent string to help with spam detection.
-                <br /><br />
+     // When the component mounts, fetch data from the API
+     componentDidMount() {          
+          RestClient.GetRequest(AppUrl.Information).then(result => {
+               // Update state with 'privacy' content from API response
+               this.setState({ privacydesc: result[0]['privacy'] });
+          }); 
+     }
 
-                An anonymized string created from your email address (also called a hash) may be provided to the Gravatar service to see if you are using it. The Gravatar service privacy policy is available here:&nbsp;
-                <a href="https://automattic.com/privacy/" target="_blank" rel="noopener noreferrer">
-                  https://automattic.com/privacy/
-                </a>. After approval of your comment, your profile picture is visible to the public in the context of your comment.
-                <br /><br />
-
-                {/* Media section */}
-                <b>Media</b> <br />
-                If you upload images to the website, you should avoid uploading images with embedded location data (EXIF GPS). Visitors to the website can download and extract any location data from images on the website.
-                <br /><br />
-
-                {/* Contact form section */}
-                <b>Contact forms</b> <br />
-                We may use contact form submission information to get in touch with you. We do not use the information submitted through contact form for marketing purposes.
-                <br /><br />
-
-                {/* Embedded content section */}
-                <b>Embedded content from other websites</b> <br />
-                Articles on this site may include embedded content (e.g. videos, images, articles, etc.). Embedded content from other websites behaves in the exact same way as if the visitor has visited the other website.
-                <br /><br />
-
-                These websites may collect data about you, use cookies, embed additional third-party tracking, and monitor your interaction with that embedded content—including tracing your interaction with the embedded content if you have an account and are logged in to that website.
-                <br />
-              </p>
-            </Col>
-          </Row>
-        </Container>
-      </Fragment>
-    )
-  }
+     // Render the component UI
+     render() {
+          return (
+               <Fragment>
+                    <Container className="mt-5">
+                         <Row>
+                              <Col lg={12} md={12} sm={12}>
+                                   {/* Title */}
+                                   <h1 className="serviceName">Privacy And Policy</h1>
+                                   <hr />
+                                   {/* Render HTML content from API */}
+                                   <p className="serviceDescription">
+                                        { parse(this.state.privacydesc) }
+                                   </p>
+                              </Col>
+                         </Row>
+                    </Container>
+               </Fragment>
+          );
+     }
 }
 
-export default PrivacyDescription
+// Export the component so it can be used in other parts of the app
+export default PrivacyDescription;
